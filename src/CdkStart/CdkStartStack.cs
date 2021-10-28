386a6cb876e433cdf7a1dc3a443c3fb57ede8364
Aziz Mohamed <aziz@azizmohamed.com>
2021-10-28 16:39:12 +1100

    initial commit

--- src/CdkStart/CdkStartStack.cs
+++ src/CdkStart/CdkStartStack.cs
@@ -1,4 +1,8 @@
+using System.Collections.Generic;
 using Amazon.CDK;
+using Amazon.CDK.AWS.EC2;
+using Amazon.CDK.AWS.ECS;
+using Amazon.CDK.AWS.ECS.Patterns;
 
 namespace CdkStart
 {
@@ -6,7 +10,30 @@ namespace CdkStart
     {
         internal CdkStartStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
-            // The code that defines your stack goes here
+            var vpc = new Vpc(this, "MyVpc", new VpcProps
+            {
+                MaxAzs = 3 // Default is all AZs in region
+            });
+
+            var cluster = new Cluster(this, "MyCluster", new ClusterProps
+            {
+                Vpc = vpc
+            });
+
+            // Create a load-balanced Fargate service and make it public
+            new ApplicationLoadBalancedFargateService(this, "MyFargateService",
+                new ApplicationLoadBalancedFargateServiceProps
+                {
+                    Cluster = cluster,          // Required
+                    DesiredCount = 6,           // Default is 1
+                    TaskImageOptions = new ApplicationLoadBalancedTaskImageOptions
+                    {
+                        Image = ContainerImage.FromAsset("src/CdkApp")
+                    },
+                    MemoryLimitMiB = 2048,      // Default is 256
+                    PublicLoadBalancer = true    // Default is false
+                }
+            );
         }
     }
 }
